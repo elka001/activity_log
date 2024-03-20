379eff866998c2eca5077f26f06fa8f16672dbef
elka001 <85257284+elka001@users.noreply.github.com>
2024-03-20 11:03:14 +0200

    Update activity.py

--- models/activity.py
+++ models/activity.py
@@ -1,17 +1,12 @@
-class Activity:
-    def __init__(self, id, name, description, duration, date, user_id):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.duration = duration
-        self.date = date
-        self.user_id = user_id
+# MODEL DEFINITIONS FOR SWAGGER DOCUMENTATION
+from flask_restx import fields
+from app import activity_ns
 
-    def to_json(self):
-        return {'id': self.id,
-                'name': self.name,
-                'description': self.description,
-                'duration': self.duration,
-                'date': self.date,
-                'user_id': self.user_id
-                }
+activity_model = activity_ns.model('Activity', {
+    'id': fields.Integer(readonly=True, description='The unique identifier of an activity.'),
+    'name': fields.String(required=True, description='The name of an activity.'),
+    'description': fields.String(required=True, description='The description of an activity.'),
+    'duration': fields.String(required=True, description='The duration of an activity.'),
+    'date': fields.String(required=True, description='The date of an activity.'),
+    'user_id': fields.Integer(required=True, description='The ID of a user this activity belongs to.')
+})
