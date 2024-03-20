037980e869c7c9653be0d3212ccb3b788713285c
elka001 <85257284+elka001@users.noreply.github.com>
2024-03-20 11:26:08 +0200

    Delete models directory

--- models/user.py
+++
@@ -1,9 +0,0 @@
-# MODEL DEFINITIONS FOR SWAGGER DOCUMENTATION
-from flask_restx import fields
-from app import user_ns
-
-user_model = user_ns.model('User', {
-    'id': fields.Integer(readonly=True, description='The unique identifier of a user.'),
-    'username': fields.String(required=True, description='The username of a user.'),
-    'email': fields.String(required=True, description='The email address of a user.')
-})
